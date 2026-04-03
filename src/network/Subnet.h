ba47413fb5bead0a5eb7b825096f4ed4d17a060f
JoseMaTzn <jmtr945@gmail.com>
2026-04-03 19:49:54 -0400

    Update Subnet.h

--- src/network/Subnet.h
+++ src/network/Subnet.h
@@ -1,20 +1,30 @@
+//
+//  Subnet.h
+//  NetworkSimulator
+//
+// 
+//
+
 #ifndef SUBNET_H
 #define SUBNET_H
 
 #include <string>
-#include <vector>
 #include "Device.h"
+#include <vector>
 
-// Subnet Class: Handles subnetting
-class Subnet
-{
+//Subnet Class: Handles subnetting
+
+class Subnet {
 public:
     std::string networkAddress;
     std::string subnetMask;
+    std::string gateway;
     std::vector<Device> devices;
-
-    Subnet(const std::string &net, const std::string &mask);
-    void addDevice(const Device &d);
+    
+    Subnet(std::string net, std::string mask, std::string gw);
+    
+    void addDevice(Device d);
+    std::string getGateway() { return gateway; }
 };
 
 #endif
