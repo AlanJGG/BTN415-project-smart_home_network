9e18ea4c3e2ac67d5ca567ef54c21ba69d69232b
k0code <mksoud226@gmail.com>
2026-04-05 15:49:17 -0400

    Update Device.cpp

--- src/network/Device.cpp
+++ src/network/Device.cpp
@@ -1,3 +1,6 @@
+// We have done all the coding by ourselves and only copied the code that our professor provided to complete our labs and projects
+// Group 10 Masoud Khire - 186174231 Jose Manuel Tiznado - 183725233 Ekjot Kaur- 189003239 Alan Joseph Guzman Gutierrez - 177035235   2026-04-05
+
 #include "Device.h"
 
 Device::Device(std::string n, std::string i, std::string m)
